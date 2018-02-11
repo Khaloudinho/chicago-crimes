eeb29a2d7f31f84c05d7a8fe16ea6c3568d0f806
Khaled Bouguettoucha <khaled.bouguettoucha5@etu.univ-lorraine.fr>
2018-02-11 14:18:13 +0100

    working on kmeans

--- src/main/java/question3/CrimesMapper.java
+++ src/main/java/question3/CrimesMapper.java
@@ -10,14 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-// 1 - Tirer k centroïdes aléatoires
-// 2 - Faire la distance euclidienne avec chaque centroïde
-// 3 - Garder ceux dont la distance avec le centroïde est <= 2 kms
-// 4 - Faire la somme des points de chaque centroïdes
-// 5 - Prendre les 3 plus criminogènes
-// 6 - Prendre les 3 moins criminogènes
-// 7 - Tracer sur la carte avec deux couleurs différentes chacunes des six zones
-
 public class CrimesMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     private List<Double[]> locations = new ArrayList<>(), centroids = new ArrayList<>();
@@ -35,35 +27,38 @@ public class CrimesMapper extends Mapper<LongWritable, Text, Text, IntWritable> 
             Double[] coordinates = new Double[2];
             coordinates[0] = Double.parseDouble(latitude);
             coordinates[1] = Double.parseDouble(longitude);
-            if (!locations.contains(coordinates)) {
-                locations.add(coordinates);
-            }
+
+            if (!locations.contains(coordinates)) locations.add(coordinates);
         }
+
     }
 
     @Override
     protected void cleanup(Context context) throws IOException, InterruptedException {
-        for (int i = 0; i < Integer.parseInt(context.getConfiguration().get(Constants.CENTROID_NUMBER_ARG)); i++) {
-            centroids.add(locations.get(rand.nextInt(locations.size())));
+
+        if (centroids.isEmpty()) {
+            for (int j = 0; j < Integer.parseInt(context.getConfiguration().get(Constants.CENTROID_NUMBER_ARG)); j++) {
+                centroids.add(locations.get(rand.nextInt(locations.size())));
+            }
         }
 
         int index = 0, number = 1;
 
         for (Double[] coordinates : locations) {
+
             double x = coordinates[0], y = coordinates[1];
 
             double minDistance = Double.MAX_VALUE;
 
-            for (int j = 0; j < centroids.size(); j++) {
-                double distance = Math.sqrt(Math.pow(centroids.get(j)[0] - x, 2) + Math.pow(centroids.get(j)[1] - y, 2));
+            for (int k = 0; k < centroids.size(); k++) {
+                double distance = Math.sqrt(Math.pow(centroids.get(k)[0] - x, 2) + Math.pow(centroids.get(k)[1] - y, 2));
                 if (distance < minDistance) {
-                    index = j;
+                    index = k;
                     minDistance = distance;
                 }
             }
 
-            context.write(new Text(index + " [" + centroids.get(index)[0] + ", " + centroids.get(index)[1] + "]"),
-                          new IntWritable(number));
+            context.write(new Text(index + " [" + centroids.get(index)[0] + ", " + centroids.get(index)[1] + "]"), new IntWritable(number));
         }
     }
 }
--- src/main/java/question3/CrimesReducer.java
+++ src/main/java/question3/CrimesReducer.java
@@ -5,29 +5,37 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
-
-// 1 - Tirer k centroïdes aléatoires
-// 2 - Faire la distance euclidienne avec chaque centroïde
-// 3 - Garder ceux dont la distance avec le centroïde est <= 2 kms
-// 4 - Faire la somme des points de chaque centroïdes
-// 5 - Prendre les 3 plus criminogènes
-// 6 - Prendre les 3 moins criminogènes
-// 7 - Tracer sur la carte avec deux couleurs différentes chacunes des six zones
 
 public class CrimesReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
     private Map<String, Integer> map;
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int sum = 0;
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) {
+        int sum = 0, counter = 0;
+
+        String centroid = "";
+
+        Double mx = 0d, my = 0d;
 
         for (IntWritable value : values) {
             sum += value.get();
+
+            String[] temp = key.toString().split(", ");
+            mx += Double.parseDouble(temp[0].substring(temp[0].indexOf("[") + 1, temp[0].length()));
+            my += Double.parseDouble(temp[1].substring(0, temp[1].indexOf("]") - 1));
+            counter++;
         }
 
-        getMap().put(key.toString(), sum);
+        mx = mx / counter;
+        my = my / counter;
+
+        if (mx != 0d && my != 0d) centroid = "[" + mx + ", " + my + "]";
+        if (!centroid.isEmpty()) getMap().put(centroid, sum);
     }
 
     @Override
